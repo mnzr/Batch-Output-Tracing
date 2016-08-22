d7dd44a0f578866cf3ffaa27a75df6833f1476cd
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-08-22 06:59:37 +0600

    Added sample Codes.txt

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 """Reads the source file and separates code examples"""
-import sys
+from sys import argv
 from extractor import Code
 
-if len(argv[])>1:
+if len(argv)>1:
     filename = argv[1]
-else
+else:
     filename = "Codes.txt"
+
 SEPARATOR = "-----"
 
 
-with open("Codes.txt", "r") as source:
+with open(filename, "r") as source:
     instance = ""
     for line in source:
         # This will hold the current code
